56d35252fda0e74c8127c5fd00c5ff6e92b56c27
Harsh Nain <naihh952@gmail.com>
2026-02-06 15:16:04 +0530

    make correct core

--- app.js
+++ app.js
@@ -22,10 +22,12 @@ import { islogin } from "./middleware/islogin.js";
 
 const app = express();
 const server = http.createServer(app);
-
 const io = new Server(server, {
     cors: {
-        origin: ["http://localhost:5173"],
+        origin: [
+            "http://localhost:5173",
+            "https://snapshot-frontend.onrender.com",
+        ],
         credentials: true,
     },
 });
@@ -39,7 +41,21 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({ origin: process.env.VITE_API_URL, credentials: true }));
+const allowedOrigins = [
+    "http://localhost:5173",
+    "https://snapshot-frontend.onrender.com",
+];
+
+app.use(cors({
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error("CORS not allowed"));
+        }
+    },
+    credentials: true,
+}));
 app.use(express.json());
 
 app.use(express.static(path.join(__dirname, "public")));
