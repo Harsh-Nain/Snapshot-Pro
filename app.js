3ef0b96c2d2495ece0506516a459255ea679ec33
Harsh Nain <naihh952@gmail.com>
2026-02-06 14:33:05 +0530

    Update fruntend url

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: process.env.VITE_API_URL, credentials: true }));
 app.use(express.json());
 
 app.use(express.static(path.join(__dirname, "public")));
