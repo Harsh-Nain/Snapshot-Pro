3e3cb3431ca577767272a1df9cfe83aff1c34708
Harsh Nain <naihh952@gmail.com>
2026-02-10 15:29:15 +0530

    Now backend correct proper working

--- controllers/message.controllers.js
+++ controllers/message.controllers.js
@@ -141,7 +141,7 @@ export const UnSend = async (req, res) => {
 
 export const ShowMessage = async (req, res) => {
 
-    const limit = 7;
+    const limit = 10;
     const page = Number(req.query.page) || 1;
     const offset = (page - 1) * limit;
 
